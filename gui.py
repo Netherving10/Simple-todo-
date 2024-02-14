43b3214646983fda88ae364ea9061de2fcca2dac
Максим Даньшин <netherving.80@gmail.com>
2024-02-15 00:17:29 +0300

    Add todo app

---
+++ gui.py
@@ -0,0 +1,57 @@
+import PySimpleGUI
+import functions
+
+label = PySimpleGUI.Text("Type in a to-do")
+input_box = PySimpleGUI.InputText(tooltip="Enter to-do", key='todo')
+add_button = PySimpleGUI.Button("Add")
+list_box = PySimpleGUI.Listbox(values=functions.get_todos(), key='todos',
+                               enable_events=True, size=[45, 10])
+edit_button = PySimpleGUI.Button("Edit")
+complete_button = PySimpleGUI.Button("Complete")
+exit_button = PySimpleGUI.Button("Exit")
+
+window = PySimpleGUI.Window("My to-do APP", 
+                            layout=[[label], [input_box, add_button],
+                                    [list_box, edit_button, complete_button],
+                                    [exit_button]],
+                            font=('Helvetica', 20)
+                            )
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+
+    if event == 'Add':
+        todos = functions.get_todos()
+        new_todo = values['todo'] + "\n"
+        todos.append(new_todo)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+    elif event == "Edit":
+        edit_todo = values['todos'][0]
+        new_todo = values['todo']
+
+        todos = functions.get_todos()
+        index = todos.index(edit_todo)
+        todos[index] = new_todo
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+    elif event == "Complete":
+        complete_todo = values['todos'][0]
+        todos = functions.get_todos()
+        index = todos.index(complete_todo)
+        todos.pop(index)
+
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+        window['todo'].update(values="")
+    elif event == 'Exit':
+        break
+    elif event == 'todos':
+        window['todo'].update(value=values['todos'][0])
+    elif event == PySimpleGUI.WIN_CLOSED:
+    # else:
+        break
+
+window.close()
